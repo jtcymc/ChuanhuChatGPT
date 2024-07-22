1b003ff895ef4db8f8dfc5712226e4f069b54df9
jtcymc <53377526+jtcymc@users.noreply.github.com>
2024-07-22 13:12:53 +0800

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -39,8 +39,8 @@ __all__ = [
 
 # 添加一个统一的config文件，避免文件过多造成的疑惑（优先级最低）
 # 同时，也可以为后续支持自定义功能提供config的帮助
-if os.path.exists("config.json"):
-    with open("config.json", "r", encoding='utf-8') as f:
+if os.path.exists("./conf/config.json"):
+    with open("./conf/config.json", "r", encoding='utf-8') as f:
         config = json.load(f)
 else:
     config = {}
